4d1975140d0fe0dff1d562b1f9e9d45168219da2
Francois Capel <fanch.capel@gmail.com>
2019-11-13 16:24:26 +0100

    yelp MVP almost functional WIP

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.destroy_all
+Restaurant.create(name: "Tching Tchong", address: "2 rue de la conciergerie", phone_number: "02-98-01-05-38", category: "chinese")
+Restaurant.create(name: "Ma doue beniguet", address: "3 rue jean Jaures", phone_number: "05-06-07-08-03", category: "french")
+Restaurant.create(name: "le flocon de sel", address: "1 rue de la montagne", phone_number: "08-09-09-12-12", category: "japanese")
+Restaurant.create(name: "La brasserie", address: "place de l'église", phone_number: "06-12-12-12-12", category: "belgian")
+Restaurant.create(name:"Le triskel", address: "3 rue de la rampe", phone_number: "02-03-04-05-06", category: "italian")
